ff65423745c6d0588546a84d9dbb762ac84f11f3
Alexander Artemenko <svetlyak.40wt@gmail.com>
2016-02-19 09:19:50 +0300

    Turn off notification from smart-mode-line about loading a color scheme.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -61,6 +61,7 @@
 ;; https://github.com/sellout/emacs-color-theme-solarized
 (load-theme 'solarized t)
 (setq sml/theme 'respectful)
+(setq sml/no-confirm-load-theme t)
 (sml/setup)
 
 (setq custom-file "~/.emacs.d/customizations.el")
