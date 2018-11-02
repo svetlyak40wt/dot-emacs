61ad4a15889bddeadce337c2da9aa98290f3030a
Alexander Artemenko <svetlyak.40wt@gmail.com>
2018-11-02 11:13:43 +0300

    Updated indentation in customizations.

--- .emacs.d/customizations.el
+++ .emacs.d/customizations.el
@@ -28,20 +28,20 @@
       ((org-agenda-ndays 21)))
      ("A" agenda ""
       ((org-agenda-skip-function
-        (lambda nil
-          (org-agenda-skip-entry-if
-           (quote notregexp)
-           "\\=.*\\[#A\\]")))
+	(lambda nil
+	  (org-agenda-skip-entry-if
+	   (quote notregexp)
+	   "\\=.*\\[#A\\]")))
        (org-agenda-ndays 1)
        (org-agenda-overriding-header "Today's Priority #A tasks: ")))
      ("u" alltodo ""
       ((org-agenda-skip-function
-        (lambda nil
-          (org-agenda-skip-entry-if
-           (quote scheduled)
-           (quote deadline)
-           (quote regexp)
-           "
+	(lambda nil
+	  (org-agenda-skip-entry-if
+	   (quote scheduled)
+	   (quote deadline)
+	   (quote regexp)
+	   "
 ]+>")))
        (org-agenda-overriding-header "Unscheduled TODO entries: ")))
      ("x" "Save to file" agenda ""
