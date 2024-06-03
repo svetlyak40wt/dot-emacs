9af4148b9bc85eb555782be049408a2b7a2dcb20
Alexander Artemenko <svetlyak.40wt@gmail.com>
2024-06-03 12:48:37 +0300

    Search ag binary in different installation paths automatically.

--- .emacs.d/lib2/12-helm.el
+++ .emacs.d/lib2/12-helm.el
@@ -9,4 +9,10 @@
     helm-ag
     :init
   (message "Configuring Helm AG")
-  (setq helm-ag-base-command "/usr/local/bin/ag --nocolor --nogroup"))
+  (cond
+   ((file-exists-p "~/.nix-profile/bin/ag")
+    (setq helm-ag-base-command "~/.nix-profile/bin/ag --nocolor --nogroup"))
+   ((file-exists-p "/usr/local/bin/ag")
+    (setq helm-ag-base-command "/usr/local/bin/ag --nocolor --nogroup"))
+   (t
+    (message "Unable to find \"ag\" command. Install it like this: nix-env -i -b silver-searcher"))))
