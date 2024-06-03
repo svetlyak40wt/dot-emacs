cb0eaeea70b89f7376757bc37560dea1e92439fb
Alexander Artemenko <svetlyak.40wt@gmail.com>
2024-06-03 13:07:01 +0300

    Add config for reverse-im package.

---
+++ .emacs.d/lib2/90-reverse-im.el
@@ -0,0 +1,22 @@
+;; This settings allows my shortcuts to work even if OS keyboard layout was
+;; switched to Russian language.
+
+;; Needed for `:after char-fold' to work
+(use-package char-fold
+  :custom
+  (char-fold-symmetric t)
+  (search-default-mode #'char-fold-to-regexp))
+
+
+(use-package reverse-im
+  :ensure t ; install `reverse-im' using package.el
+  :demand t ; always load it
+  :after char-fold ; but only after `char-fold' is loaded
+  :bind
+  ("M-T" . reverse-im-translate-word) ; fix a word in wrong layout
+  :custom
+  (reverse-im-char-fold t) ; use lax matching
+  (reverse-im-read-char-advice-function #'reverse-im-read-char-include)
+  (reverse-im-input-methods '("russian-computer")) ; translate these methods
+  :config
+  (reverse-im-mode t)) ; turn the mode on
