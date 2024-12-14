c71950144cf4cfb20e5151504aaa439119dc3e09
Alexander Artemenko <svetlyak.40wt@gmail.com>
2024-12-14 09:06:53 +0000

    Store undo tree files in the separate directory.

--- .emacs.d/lib2/21-undo-tree.el
+++ .emacs.d/lib2/21-undo-tree.el
@@ -1,4 +1,8 @@
 (use-package undo-tree
   :ensure t
   :config
-  (global-undo-tree-mode t))
+  (global-undo-tree-mode t)
+  ;; To not clutter my projects with
+  ;; .*.~undo-tree~ files:
+  (setf undo-tree-history-directory-alist
+        '((".*" . ".emacs.d/undo-history/"))))
