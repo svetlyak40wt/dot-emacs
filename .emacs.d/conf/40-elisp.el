bdc75f917be7973c188171a4b72249ab485c1a86
Alexander Artemenko <svetlyak.40wt@gmail.com>
2025-11-18 11:48:59 +0300

    New configs for term and elisp.

---
+++ .emacs.d/conf/40-elisp.el
@@ -0,0 +1,16 @@
+;; (use-package macrostep
+;;   ;; :defer t
+;;   ;; (define-key emacs-lisp-mode-map (kbd "C-c e") 'macrostep-expand)
+;;   )
+
+
+;; (eval-after-load
+;;     (define-key emacs-lisp-mode-map (kbd "C-c e") 'macrostep-expand))
+
+;; (use-package 
+;;   ;; :ensure t
+;;   :defer t
+;;   :mode "\\.el\\'"
+;;   :bind (:map emacs-lisp-mode-map
+;;               ("C-c RET" . macrostep-expand)))
+
